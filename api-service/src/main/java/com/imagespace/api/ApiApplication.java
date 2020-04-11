7328846745dd768558139f31967a3f71b7dd4c79
Kovtiuk, Robert [External] <Robert.Kovtiuk@externals.adidas.com>
2020-04-12 01:05:28 +0300

    IS-14 Source-service: minor fixes

--- api-service/src/main/java/com/imagespace/api/ApiApplication.java
+++ api-service/src/main/java/com/imagespace/api/ApiApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.kafka.annotation.EnableKafka;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableKafka
 @SpringBootApplication
 public class ApiApplication {
 
